cbfa42ce671af4af0e0ace0bf3e8d079853e6b75
joaoteles <joao.teles@zencloud.com.br>
2023-09-20 21:12:25 -0300

    :sparkles: feat: create-invitation controller

---
+++ src/http/controller/create-invitation.ts
@@ -0,0 +1,41 @@
+import { z } from "zod";
+import { FastifyRequest, FastifyReply } from "fastify";
+import { makeCreateInvitationUseCase } from "@/use-case/factories/make-create-invitation-use-case";
+import { InvalidCredentialsError } from "@/use-case/erros/invalid-credentials-error";
+
+export async function invite(request: FastifyRequest, reply: FastifyReply) {
+  const inviteBodySchema = z.object({
+    eventName: z.string(),
+    descriptionEvent: z.string(),
+    userEmail: z.string(),
+    toEmail: z.string(),
+    dateTime: z.string(),
+    meetingTime: z.string(),
+  });
+
+  try {
+    const {
+      eventName,
+      descriptionEvent,
+      userEmail,
+      toEmail,
+      dateTime,
+      meetingTime,
+    } = inviteBodySchema.parse(request.body);
+
+    const createInvitationUseCase = makeCreateInvitationUseCase();
+    await createInvitationUseCase.execute({
+      eventName,
+      descriptionEvent,
+      userEmail,
+      toEmail,
+      dateTime,
+      meetingTime,
+    });
+    return reply.status(201).send();
+  } catch (error) {
+    if (error instanceof InvalidCredentialsError)
+      return reply.status(409).send({ message: error.message });
+    throw error;
+  }
+}
