719a383868b596c35e7e04cadb4946a5dcddc23d
joaoteles <joao.teles@zencloud.com.br>
2023-09-20 21:03:12 -0300

    :sparkles: feat: interfaces file

---
+++ src/interface/IData.ts
@@ -0,0 +1,16 @@
+interface IData {
+  text: string;
+  eventName: string;
+  userEmail: string;
+  toEmail: string;
+}
+
+export interface IEventData extends IData {
+  dateTime: string;
+  meetingTime: string;
+}
+
+export interface IEmailData extends IData {
+  icsFileName?: string;
+  icsContent?: string;
+}
