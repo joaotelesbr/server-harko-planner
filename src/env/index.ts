74da38d92cc7c51f79cbdb0143d6129d803b28de
joaoteles <joao.teles@zencloud.com.br>
2023-09-20 21:06:59 -0300

    :recycle: refactor: add email and password to envSchema

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,16 +1,18 @@
-import 'dotenv/config'
-import { z } from 'zod'
+import "dotenv/config";
+import { z } from "zod";
 
 const envSchema = z.object({
-  NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
+  NODE_ENV: z.enum(["dev", "test", "production"]).default("dev"),
   PORT: z.coerce.number().default(3333),
-})
+  DATABASE_EMAIL: z.string(),
+  DATABASE_PASSWORD: z.string(),
+});
 
-const _env = envSchema.safeParse(process.env)
+const _env = envSchema.safeParse(process.env);
 
 if (_env.success === false) {
-  console.error(`❌ invalid environment variables! ❌`, _env.error.format())
-  throw new Error('invalid environment variables!')
+  console.error(`❌ invalid environment variables! ❌`, _env.error.format());
+  throw new Error("invalid environment variables!");
 }
 
-export const env = _env.data
+export const env = _env.data;
